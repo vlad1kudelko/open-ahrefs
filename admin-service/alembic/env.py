38ed760163412cc28b4af4b700b0fec063599bca
vir <you@example.com>
2025-12-15 16:39:45 +0800

    pre

--- admin-service/alembic/env.py
+++ admin-service/alembic/env.py
@@ -1,12 +1,9 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
-
 from app.config.settings import settings
 from app.db.models import Base
+from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,7 +14,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option('sqlalchemy.url', settings.database_url_psycopg)
+config.set_main_option("sqlalchemy.url", settings.database_url_psycopg)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -69,9 +66,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
