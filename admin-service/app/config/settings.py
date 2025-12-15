c6370c4b69fa359cb1dcb1c11ded388571243830
Vlad Kudelko <vlad1kudelko@gmail.com>
2025-12-15 13:10:49 +0800

    isort

--- admin-service/app/config/settings.py
+++ admin-service/app/config/settings.py
@@ -1,5 +1,6 @@
-from dotenv import find_dotenv
 from functools import lru_cache
+
+from dotenv import find_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
--- admin-service/app/db/engine.py
+++ admin-service/app/db/engine.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from config.settings import settings
+from ..config.settings import settings
 
 engine = create_engine(
     url=settings.database_url_psycopg,
