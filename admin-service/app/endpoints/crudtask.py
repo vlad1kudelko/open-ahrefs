292301773410715d6f13127619546034091a9ea5
Vlad Kudelko <vlad1kudelko@gmail.com>
2025-12-15 19:57:12 +0800

    crudtask

---
+++ admin-service/app/endpoints/crudtask.py
@@ -0,0 +1,32 @@
+from urllib.parse import urlparse
+
+from app.db.engine import session_factory
+from app.db.models import Url
+from fastapi import APIRouter
+
+crudtask = APIRouter()
+
+
+@crudtask.get("/addurl")
+def api_addurl(domain: str):
+    if not urlparse(domain).scheme:
+        return 1
+    if not urlparse(domain).netloc:
+        return 2
+    with session_factory() as session:
+        item_url = Url(
+            scheme=urlparse(domain).scheme,
+            domain=urlparse(domain).netloc.split(":")[0],
+        )
+        if len(urlparse(domain).netloc.split(":")) == 2:
+            item_url.port = int(urlparse(domain).netloc.split(":")[1])
+        if urlparse(domain).path:
+            item_url.path = urlparse(domain).path
+        if urlparse(domain).query:
+            item_url.param = urlparse(domain).query
+        if urlparse(domain).fragment:
+            item_url.anchor = urlparse(domain).fragment
+        session.add(item_url)
+        # add kafka
+        #  session.commit()
+        session.flush()
