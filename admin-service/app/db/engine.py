b34cb40e27dd9d4e9c090214271fa41d3d517bc8
vir <you@example.com>
2025-12-15 19:57:12 +0800

    crudtask

--- admin-service/app/db/engine.py
+++ admin-service/app/db/engine.py
@@ -1,11 +1,10 @@
+from app.config.settings import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from ..config.settings import settings
-
 engine = create_engine(
     url=settings.database_url_psycopg,
-    # echo=True,
+    echo=True,
 )
 
 session_factory = sessionmaker(bind=engine)
--- admin-service/app/main.py
+++ admin-service/app/main.py
@@ -1,8 +1,11 @@
+from app.endpoints.crudtask import crudtask
 from fastapi import FastAPI
 
 app = FastAPI()
 
+app.include_router(crudtask, prefix="/crudtask")
 
-@app.get("/addurl")
-async def api_addurl(url: str):
-    return {"message": url}
+
+@app.get("/")
+def index():
+    return "Welcome to the 'open-ahrefs' project"
