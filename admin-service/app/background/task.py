859749526bc1c791ce28d3f9738a512880511fb2
Vlad Kudelko <vlad1kudelko@gmail.com>
2025-12-16 20:30:28 +0800

    aiokafka

--- admin-service/app/background/task.py
+++ admin-service/app/background/task.py
@@ -1,7 +1,8 @@
+import asyncio
 from datetime import datetime, timezone
 
+from aiokafka import AIOKafkaProducer
 from config.settings import settings
-from confluent_kafka import Producer
 from db.engine import session_factory
 from db.models import Url
 
@@ -18,21 +19,24 @@ def compile_domain(obj: Url) -> str:
 
 
 async def pipe_push():
-    with session_factory() as session:
-        conf = {
-            "bootstrap.servers": settings.kafka_url,
-            "client.id": "admin-service",
-        }
-        producer = Producer(conf)
-        # сейчас простой запрос на "хотя бы один парсинг"
-        # TODO потом переписать на сложный "хотя бы один парсинг за последние Х дней"
-        # но с сохранением идемпотентности
-        for item in session.query(Url).filter(Url.last_pars.is_(None)).all():
-            kafka_item = kafka_models.Url(url_id=item.url_id, url=compile_domain(item))
-            message = kafka_item.model_dump_json()
-            producer.produce(topic="topic_url", value=message.encode("utf-8"))
-            item.last_pars = datetime.now(timezone.utc)
-            producer.poll(0)
-            session.flush()
-        producer.flush()
-        session.commit()
+    producer = AIOKafkaProducer(bootstrap_servers=settings.kafka_url)
+    await producer.start()
+    try:
+        with session_factory() as session:
+            tasks: list[asyncio.Future] = []
+            # сейчас простой запрос на "хотя бы один парсинг"
+            # TODO потом переписать на сложный "хотя бы один парсинг за последние Х дней"
+            # но с сохранением идемпотентности
+            for item in session.query(Url).filter(Url.last_pars.is_(None)).limit(100):
+                kafka_item = kafka_models.Url(
+                    url_id=item.url_id, url=compile_domain(item)
+                )
+                message = kafka_item.model_dump_json().encode("utf-8")
+                task = await producer.send("topic_url", message)
+                tasks.append(task)
+                item.last_pars = datetime.now(timezone.utc)
+            if tasks:
+                await asyncio.gather(*tasks)
+            session.commit()
+    finally:
+        await producer.stop()
