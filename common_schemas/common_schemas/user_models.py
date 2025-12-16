38fb45ea8ea9ee310a70b4b3fa8d5c95a8f7c230
vir <you@example.com>
2025-12-16 14:21:19 +0800

    bg

---
+++ common_schemas/common_schemas/user_models.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+
+class Url(BaseModel):
+    _id: int
+    url: str
+
+
+class Link(BaseModel):
+    tag: str
+    attr: str | None
+    field: str
+
+
+class Res(BaseModel):
+    url_id: int
+    status_code: int
+    h1: str | None
+    title: str | None
+    description: str | None
+    canonical: str | None
+    redirect: str | None
+    links: [Link]
