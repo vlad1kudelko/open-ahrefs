b10a561446d215fc9d8f004ef788e27f082d0e5d
vir <you@example.com>
2025-12-17 09:06:35 +0800

    scraper

---
+++ scraper-service/app/config/settings.py
@@ -0,0 +1,32 @@
+from functools import lru_cache
+
+from dotenv import find_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    PGADMIN_EMAIL: str
+    PGADMIN_PASSWORD: str
+    KAFKA_HOST: str
+    KAFKA_PORT: str
+
+    model_config = SettingsConfigDict(
+        env_file=find_dotenv(),
+    )
+
+    @property
+    def kafka_url(self) -> str:
+        return f"{self.KAFKA_HOST}:{self.KAFKA_PORT}"
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
