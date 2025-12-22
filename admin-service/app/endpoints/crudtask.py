b523801b21cfe6f692809133a65fb04dee5796be
vir <you@example.com>
2025-12-22 14:42:22 +0800

    -

--- admin-service/app/endpoints/crudtask.py
+++ admin-service/app/endpoints/crudtask.py
@@ -1,5 +1,5 @@
 from db.engine import async_session_factory
-from db.models import Url
+from db.models import Domain, Url
 from fastapi import APIRouter
 from sqlalchemy import select
 from tools.url_to_obj import url_to_obj
@@ -13,6 +13,7 @@ async def api_addurl(url: str) -> str:
     if obj_url is None:
         return "ERROR parse"
     async with async_session_factory() as session:
+        session.add(Domain(domain=obj_url.hostname))
         stmt = select(Url).where(Url.url_hash == obj_url.url_hash)
         result = await session.execute(stmt)
         old_url: Url | None = result.scalar_one_or_none()
